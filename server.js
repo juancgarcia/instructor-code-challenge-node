c28cc775ae792abc7976707e3d745eba1efe2c68
juancgarcia <juangarcia.info@gmail.com>
2017-01-16 13:56:31 -0500

    server.js fixes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 
@@ -7,18 +8,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
-    return
+    return;
+  }
   
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
@@ -27,6 +29,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
