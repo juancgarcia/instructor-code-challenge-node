5688f1cc9c65a29e2ff191113eb3be4219cea85b
juancgarcia <juangarcia.info@gmail.com>
2017-01-17 12:05:28 -0500

    optional server defined http port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -29,6 +31,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(app.get('port'), function(){
+  console.log("Listening on port "+app.get('port'));
 });
