fcc57c4b904101c2763020d01cdefa8f4c89d4e2
juancgarcia <juangarcia.info@gmail.com>
2017-01-18 01:12:11 -0500

    detail Favorite checkbox

--- server.js
+++ server.js
@@ -13,23 +13,84 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
+  var input = req.query;
+  if(input.name || input.oid){
+
+    var results = searchData(input);
+
+    res.setHeader('Content-Type', 'application/json');
+    if(results.length > 0)
+      res.send(JSON.stringify(results[0]));
+    else
+      res.send("{}");
+
+    return;
+  }
+
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
+app.post('/favorites', function(req, res){
+  var input = req.body;
+  console.log('POST');
+  console.log(JSON.stringify(input, null, 4));
+
+  if(!input.name || !input.oid){
     res.send("Error");
     return;
   }
-  
+
+  var results = searchData(input);
   var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
+
+  if(results.length == 0)
+    data.push(input);
+  
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  res.send(JSON.stringify(input));
 });
+
+app.delete('/favorites', function(req, res){
+  var input = req.query;
+  console.log('POST');
+  console.log(JSON.stringify(input, null, 4));
+
+  if(/*!input.name ||*/ !input.oid){
+    res.send("Error");
+    return;
+  }
+
+  var reduced = reducedData(input);
+  
+  fs.writeFile('./data.json', JSON.stringify(reduced));
+  res.setHeader('Content-Type', 'application/json');
+  res.send('{}');
+});
+
+var reducedData = function(input){
+  input = input || {};
+
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  var remove = function(x){
+    return (input.oid == x.oid) ? false : x;
+  };
+
+  return data.map(remove).filter(function(x){return x;});
+};
+
+var searchData = function(input){
+  input = input || {};
+
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  var search = function(x){
+    return (input.oid == x.oid) ? x : false;
+  };
+
+  return data.map(search).filter(function(x){return x;});
+};
 
 app.listen(app.get('port'), function(){
   console.log("Listening on port "+app.get('port'));
